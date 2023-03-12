e853db97107172536c70d52798e319fe56a73b0d
Vyacheslav Makarov <orchara69@gmail.com>
2023-03-12 21:23:41 +0500

    add_task() new iteration implemented work() first iteration implemented

--- threading/main.cpp
+++ threading/main.cpp
@@ -1,19 +1,68 @@
 #include <iostream>
 #include <exception>
-
+#include <type_traits>
 #include "pooler.hpp"
 
 
+int result = 0;
+template <typename F>
+struct test{
+    F func;
+};
+
+void twice(int x, int& res){
+    res = x * 2;
+}
+
+int add_five(int x, int& res){
+    res = x + 5;
+    return res;
+}
+
+    template <typename _Fn, typename... _Args>
+    void fu(bool ret, _Fn&& func, _Args&&... ag){
+        
+        std::function<void()> fc;
+        
+        auto tmp = std::bind(std::forward<_Fn>(func), std::forward<_Args>(ag)...);
+        //if(ret){
+        //    fc = [tmp](){auto res = tmp();};
+        //} else {
+            fc = [tmp](){tmp();};
+        //}
+        fc();
+}
+
 int main(){
     try{
-        std::function<void(int)> func = [] (int x) {std::cout << x << std::endl;};
+        
+        std::function<void(int, int&)> func = [] (int x, int& res) {res = x * x;};
+        
+        //auto tmp = std::bind<int>(t_func_2, 10);
+        //auto tmp = std::bind(t_func_1, 10);
+        
+        // fu(true, t_func_2, 10);
+        // fu(false, t_func_1, 20);
+        // typedef std::result_of<decltype(t_func_2)&(int)>::type ret_type;
+        // auto tmp = std::bind<ret_type>(t_func_2, 50);
+        // int test = tmp();
+        std::cout << "result is: " << result << std::endl;
 
         ThreadPool trd(1);
+        std::vector<int> r(4, 0);
+        // trd.add_task(func, 3000, std::ref(res[0]));
+        // trd.add_task(func, 30, std::ref(res[1]));
+        trd.add_task(add_five, 10, std::ref(r[2]));
+        trd.add_task(twice, 20, std::ref(r[3]));
+        //trd.start();
+        trd.stop();
 
-        trd.add_task(func, 3000);
-        //trd.add_task(func, 30);
-        trd.start();
+        // std::thread trd1(add_five, 10, std::ref(res[2]));
+        // trd1.join();
 
+        for(auto i : r){
+            std::cout << i << std::endl;
+        }
         return 0;
     }
     catch (std::exception err){
--- threading/pooler.hpp
+++ threading/pooler.hpp
@@ -2,54 +2,73 @@
 
 #include <functional>
 #include <thread>
+#include <condition_variable>
 #include <vector>
+#include <mutex>
 #include <list>
 
 
 class ThreadPool{
 private:
     size_t size;
-    bool is_runing;
+    std::atomic<bool> is_runing;
     std::vector<std::thread> threads;
-    std::list<std::thread> queue;
+    std::list<std::function<void()>> queue;
+    std::mutex queue_lock;
+    std::condition_variable queue_check;
+
 
 public:
     ThreadPool(size_t s){
+        is_runing = true;
         size = s;
+        for(size_t i = 0; i < s; i++){
+            
+            threads.emplace_back(&ThreadPool::work, this);
+        }
+    }
+
+    ~ThreadPool(){
+        //Join();
     }
 
 
-    template <typename _Fn, typename... __Args>
-    size_t add_task(_Fn&& func, __Args&&... ag){
-        std::thread trd(std::forward<_Fn>(func), std::forward<__Args>(ag)...);
-        if(threads.size() < size){
-            threads.push_back(std::move(trd));
-        } else {
-            queue.push_back(std::move(trd));
-        }
-        
+    template <typename _Fn, typename... _Args>
+    size_t add_task(_Fn&& func, _Args&&... ag){
+        auto tmp = std::bind(std::forward<_Fn>(func), std::forward<_Args>(ag)...);
+        std::lock_guard<std::mutex> q_lock(queue_lock);
+        queue.push_back([&func, &ag...](){std::bind(std::forward<_Fn>(func), std::forward<_Args>(ag)...)();});
+        queue_check.notify_one();
         return 0;
     }
 
 
-    void start(){
+    void stop(){
+        is_runing = false;
         Join();
     }
     
 private:
-    void Detach(){
-        for(auto& trd : threads){
-            if(trd.joinable()){
-                trd.detach();
-            }
-        }
-    }
-
     void Join(){
         for(auto& trd : threads){
             if(trd.joinable()){
                 trd.join();
             }
         }
+    }
+
+    void work(){
+        while(is_runing){
+            std::unique_lock<std::mutex> q_lock(queue_lock);
+            queue_check.wait(q_lock, [this](){return !queue.empty();});
+            if(!queue.empty()){
+                auto task = std::move(queue.front());
+                queue.pop_front();
+                q_lock.unlock();
+
+                task();
+            }
+        }
+        return;
     }
 };
