9b6d7a53c027f23b6c28a6c4ccf0f9adb9745d0f
Makarov Vyacheslav <orchara69@gmail.com>
2023-03-13 09:17:51 +0500

    wait_all() implemented Join() call moved to destructor

--- threading/pooler.hpp
+++ threading/pooler.hpp
@@ -5,6 +5,7 @@
 #include <condition_variable>
 #include <vector>
 #include <mutex>
+#include <atomic>
 #include <list>
 
 
@@ -14,8 +15,8 @@ private:
     std::atomic<bool> is_runing;
     std::vector<std::thread> threads;
     std::list<std::function<void()>> queue;
-    std::mutex queue_lock;
-    std::condition_variable queue_check;
+    std::mutex queue_lock, state_lock;
+    std::condition_variable queue_check, work_state;
 
 
 public:
@@ -23,13 +24,12 @@ public:
         is_runing = true;
         size = s;
         for(size_t i = 0; i < s; i++){
-            
             threads.emplace_back(&ThreadPool::work, this);
         }
     }
 
     ~ThreadPool(){
-        //Join();
+        Join();
     }
 
 
@@ -37,15 +37,23 @@ public:
     size_t add_task(_Fn&& func, _Args&&... ag){
         auto tmp = std::bind(std::forward<_Fn>(func), std::forward<_Args>(ag)...);
         std::lock_guard<std::mutex> q_lock(queue_lock);
-        queue.push_back([&func, &ag...](){std::bind(std::forward<_Fn>(func), std::forward<_Args>(ag)...)();});
+        queue.push_back([tmp](){tmp();});
         queue_check.notify_one();
         return 0;
     }
 
 
+    void wait_all(){
+        std::unique_lock<std::mutex> s_lock(state_lock);
+        work_state.wait(s_lock, [this]()->bool{
+            std::lock_guard<std::mutex> q_lock(queue_lock);
+            return queue.empty();
+        });
+    }
+
     void stop(){
         is_runing = false;
-        Join();
+        queue_check.notify_all();        
     }
     
 private:
@@ -60,15 +68,17 @@ private:
     void work(){
         while(is_runing){
             std::unique_lock<std::mutex> q_lock(queue_lock);
-            queue_check.wait(q_lock, [this](){return !queue.empty();});
+            queue_check.wait(q_lock);
+            
             if(!queue.empty()){
                 auto task = std::move(queue.front());
                 queue.pop_front();
                 q_lock.unlock();
-
                 task();
+                work_state.notify_all();
             }
         }
+        std::cout << "thread " << std::this_thread::get_id() << " closed up\n";
         return;
     }
 };
