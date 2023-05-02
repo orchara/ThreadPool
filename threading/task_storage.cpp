fcb0c982336cf3ea0a74fe2da2541d746d72b55b
Makarov Vyacheslav <orchara69@gmail.com>
2023-05-02 11:59:38 +0500

    first view plan of TaskStorage

--- threading/task_storage.cpp
+++ threading/task_storage.cpp
@@ -1,7 +1,31 @@
 #pragma once
 
-#include<atomic>
+#include <condition_variable>
 
 class TaskStorage {
- 
+public:
+    //...
+private:
+    //task
+    std::condition_variable check_done;
+    //result
+public:
+    template<typename _Fn, typename... T>
+    TaskStorage(_Fn func, T... ag){
+        // set_task(func, ag...)
+    }
+    // auto get_result()
+    // void wait()
+    // T get_task()
+
+private:
+    // template<typename _Fn, typename... T>
+    // class Task{
+        // void set_task(_Fn func, T... ag) { //all
+    //  }
+    // };
+
+    // template<typename... T>
+    // class Task <std::function<void(T...)>, T...> {
+        // void set_task(std::function<void(T...)> func, T... ag) //void
 };
